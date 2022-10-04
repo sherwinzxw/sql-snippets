8638e6cc6ecf3106b8d3d598aaebafa81dc25845
Sherwin Zhao <sherwin.zhao@justice.tas.gov.au>
2022-10-04 16:20:57 +1100

    Add sql sp ddl to support Synapse governance

---
+++ procedures/SYNAPSE/dbo.usp_DynamicSQL_DropExternalTables.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE dbo.usp_DynamicSQL_DropExternalTables 
+    @TargetSchema VARCHAR(255) 
+AS 
+    DECLARE @sql NVARCHAR(max) = '';
+    SELECT
+        @sql += ' DROP EXTERNAL TABLE ' + QUOTENAME(S.name) + '.' + QUOTENAME(E.name) + '; '
+    FROM
+        sys.external_tables E
+        LEFT JOIN sys.schemas S ON E.schema_id = S.schema_id
+    WHERE
+        S.name = @ERRORTargetSchema;
+    
+    EXEC @sql;
